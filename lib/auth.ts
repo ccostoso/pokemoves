f5d9b3ee916d2ee20f1fab2a5498f67027ddbeeb
Carlos Costoso <carlos.costoso@outlook.com>
2026-06-25 15:52:26 -0400

    Adds Prisma and BetterAuth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,22 @@
-import { betterAuth } from "better-auth"
-import { Pool } from "pg"
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-    database: new Pool({
-        // connection options
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "sqlite", ...etc
     }),
     secret: process.env.BETTER_AUTH_SECRET,
     url: process.env.BETTER_AUTH_URL,
+    emailAndPassword: {
+        enabled: true,
+    },
+});
 
+import { createAuthClient } from "better-auth/react"
+export const authClient = createAuthClient({
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: process.env.BETTER_AUTH_URL,
 })
+
+export const { signIn, signUp, useSession } = createAuthClient()
---
+++ lib/prisma.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+// This file is a singleton for the Prisma client, which is used to connect to the database. 
+// We use a singleton to avoid creating multiple instances of the Prisma client, which can cause 
+// issues with database connections.
+const globalForPrisma = globalThis as typeof globalThis & {
+    prisma?: PrismaClient
+}
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+
+// Create a new Prisma client instance if one doesn't already exist, and store it in the global object
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter, log: ["error"] })
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+export { prisma }
