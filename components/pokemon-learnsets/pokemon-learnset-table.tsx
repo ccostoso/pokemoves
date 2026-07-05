24957427af1a6867e5af57bd11279314b192e195
Carlos Costoso <carlos.costoso@outlook.com>
2026-07-05 00:41:45 -0400

    Fixes bug where no move name is shown if API breaks and does not send move names

--- components/pokemon-learnsets/pokemon-learnset-table.tsx
+++ components/pokemon-learnsets/pokemon-learnset-table.tsx
@@ -4,6 +4,7 @@ import TypeSprite from "../type-sprite"
 type Move = {
     level: number,
     move: {
+        name: string,
         type: {
             name: string
         },
@@ -26,13 +27,20 @@ export default function PokemonLearnsetTable({ pokemonMoves }: PokemonLearnsetTa
                 </TableRow>
             </TableHeader>
             <TableBody>
-                { pokemonMoves.map((move: Move) => (
-                    <TableRow key={ `${move.move.movenames[0]?.name}-${move.level}` }>
-                        <TableCell className="text-center">{ move.level }</TableCell>
-                        <TableCell>{ move.move.movenames[0]?.name }</TableCell>
-                        <TableCell>{ move.move.type.name && <TypeSprite typeName={ move.move.type.name } /> }</TableCell>
-                    </TableRow>
-                )) }
+                { pokemonMoves.map((move: Move, index: number) => {
+                    const displayName = move.move.movenames[0]?.name
+                    const apiName = move.move.name
+                    const viewedName = displayName ?? apiName ?? "Unknown move"
+                    const keyBase = displayName ?? apiName
+
+                    return (
+                        <TableRow key={ keyBase ? `${keyBase}-${move.level}` : `unknown-${move.level}-${index}` }>
+                            <TableCell className="text-center">{ move.level }</TableCell>
+                            <TableCell>{ viewedName }</TableCell>
+                            <TableCell>{ move.move.type.name && <TypeSprite typeName={ move.move.type.name } /> }</TableCell>
+                        </TableRow>
+                    )
+                }) }
             </TableBody>
         </Table>
     )
